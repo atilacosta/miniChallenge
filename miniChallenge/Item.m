f3a5c5cc5eb5ab0893cd7c7179508f53a23f5364
atilacosta <rcosta.atila@gmail.com>
2017-04-19 17:37:36 -0300

    collection

---
+++ miniChallenge/Item.m
@@ -0,0 +1,13 @@
+//
+//  Item.m
+//  miniChallenge
+//
+//  Created by Henrique Jordão on 19/04/17.
+//  Copyright © 2017 Atila Costa. All rights reserved.
+//
+
+#import "Item.h"
+
+@implementation Item
+
+@end
