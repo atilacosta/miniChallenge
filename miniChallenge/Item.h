d2847edd2bdca5efcf2f7dd689a55219c9a6896b
Henrique Jordão <hcjordao@Henrique-Capelattos-MacBook-Pro.local>
2017-04-19 15:38:18 -0300

    Adicionei a clsse item relacionada GameViewController

---
+++ miniChallenge/Item.h
@@ -0,0 +1,13 @@
+//
+//  Item.h
+//  miniChallenge
+//
+//  Created by Henrique Jordão on 19/04/17.
+//  Copyright © 2017 Atila Costa. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Item : NSObject
+
+@end
