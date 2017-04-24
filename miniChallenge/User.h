b4aa97312914db08b452cda8a3644f65999ffb63
gmhertz <gmhertz@gunters-macbook-pro.local>
2017-04-24 16:10:38 -0300

    user class done

--- miniChallenge/User.h
+++ miniChallenge/User.h
@@ -2,12 +2,33 @@
 //  User.h
 //  miniChallenge
 //
-//  Created by Günter Hertz on 24/04/17.
+//  Created by Günter Hertz on 19/04/17.
 //  Copyright © 2017 Atila Costa. All rights reserved.
 //
 
 #import <Foundation/Foundation.h>
 
 @interface User : NSObject
+
+
+@property (nonatomic, readonly)NSNumber *userId;
+@property (nonatomic)NSNumber *playerPoints;
+@property (nonatomic, readonly)NSNumber *numberOfCharacters;
+@property (nonatomic, readonly, copy)NSSet *answeredQuestionsIds;
+@property (nonatomic, readonly, copy)NSDictionary *userSettings;
+
+
+
+- (instancetype)initWithUserId:(NSNumber *)userId;
+
+- (void)insertAnsweredQuestionsId:(NSNumber *)questionId;
+
+- (void)changeSoundStatus;
+
+- (void)changeMusicStatus;
+
+-(void)addCharacter;
+
+
 
 @end
