cf616cd6c14f0de67f34f54e3963fc80bf589bf3
gmhertz <gmhertz@Gunters-MacBook-Pro.local>
2017-04-24 16:07:00 -0300

    user class done

---
+++ miniChallenge/User.h
@@ -0,0 +1,13 @@
+//
+//  User.h
+//  miniChallenge
+//
+//  Created by Günter Hertz on 24/04/17.
+//  Copyright © 2017 Atila Costa. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface User : NSObject
+
+@end
---
+++ miniChallenge/User.m
@@ -0,0 +1,13 @@
+//
+//  User.m
+//  miniChallenge
+//
+//  Created by Günter Hertz on 24/04/17.
+//  Copyright © 2017 Atila Costa. All rights reserved.
+//
+
+#import "User.h"
+
+@implementation User
+
+@end
