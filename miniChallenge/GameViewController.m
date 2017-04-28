f9b79259d24d5a18a30b8964aacefff9e03702e7
Henrique Jordão <henriquejordao@henriques-macbook-pro.local>
2017-04-28 17:13:25 -0300

    28/04 - Henrique

--- miniChallenge/GameViewController.m
+++ miniChallenge/GameViewController.m
@@ -13,30 +13,19 @@
 
 @interface GameViewController ()
 
-@property (weak, nonatomic) IBOutlet UIView *subjectSelectionView;
-@property (weak, nonatomic) IBOutlet UIView *subjectOptionsView;
-
 @property (weak, nonatomic) IBOutlet UILabel *selectedItemName;
 @property (weak, nonatomic) IBOutlet UILabel *selectedItemDescription;
 
-@property (weak, nonatomic) IBOutlet UIButton *selectedItemSubject1;
-@property (weak, nonatomic) IBOutlet UIButton *selectedItemSubject2;
-@property (weak, nonatomic) IBOutlet UIButton *selectedItemSubject3;
-@property (weak, nonatomic) IBOutlet UIButton *selectedItemSubject4;
-@property (weak, nonatomic) IBOutlet UIButton *selectedItemSubject5;
-@property (weak, nonatomic) IBOutlet UIButton *selectedItemSubject6;
+@property (weak, nonatomic) IBOutlet UIButton *selectedSubjectOne;
+@property (weak, nonatomic) IBOutlet UIButton *selectedSubjectTwo;
+@property (weak, nonatomic) IBOutlet UIButton *selectedSubjectThree;
+@property (weak, nonatomic) IBOutlet UIButton *selectedSubjectFour;
+@property (weak, nonatomic) IBOutlet UIButton *selectedSubjectFive;
+@property (weak, nonatomic) IBOutlet UIButton *selectedSubjectSix;
 
+@property (weak, nonatomic) IBOutlet UIView *subjectView;
 
-@property(nonatomic) UIView *dismissView;
-
-
-//@property (weak, nonatomic) IBOutlet UILabel *selectedQuestionText;
-//@property (weak, nonatomic) IBOutlet UIButton *selectedQuestionAlternative1;
-//@property (weak, nonatomic) IBOutlet UIButton *selectedQuestionAlternative2;
-//@property (weak, nonatomic) IBOutlet UIButton *selectedQuestionAlternative3;
-//@property (weak, nonatomic) IBOutlet UIButton *selectedQuestionAlternative4;
-//@property (weak, nonatomic) IBOutlet UIButton *selectedQuestionAlternative5;
-
+@property (nonatomic) NSArray *buttonArray;
 
 @end
 
@@ -58,131 +47,152 @@
         [current addTarget:self action:@selector(itemPressed:) forControlEvents:UIControlEventTouchUpInside];
     }
     
-    NSLog(@"%@",self.selectedScene.itemsList[0].itemSubjects[0].subjectName);
+    self.subjectView.hidden = YES;
     
-    //self.subjectSelectionView.userInteractionEnabled = NO;
-    //self.subjectOptionsView.userInteractionEnabled = NO;
+    self.buttonArray = [[NSArray alloc]initWithObjects:self.selectedSubjectOne, self.selectedSubjectTwo,self.selectedSubjectThree,self.selectedSubjectFour,self.selectedSubjectFive, self.selectedSubjectSix, nil];
     
-    UITapGestureRecognizer *tap = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(dismissEverything)];
-    tap.delegate = self;
-    [self.view addGestureRecognizer:tap];
-    
-    [self clearSubjectViewContent];
-
-    
-
 }
 
-- (BOOL) gestureRecognizer:(UIGestureRecognizer *)gestureRecognizer shouldReceiveTouch:(UITouch *)touch
-{
-    if (CGRectContainsPoint(self.subjectSelectionView.bounds, [touch locationInView:self.subjectSelectionView]))
-        return NO;
-    
-    return YES;
-}
-
--(void) dismissEverything {
-    self.subjectSelectionView.hidden = YES;
-    self.dismissView.hidden = YES;
-    [self clearSubjectViewContent];
-}
-
--(void) doNothing {
-}
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
 
-- (IBAction)itemPressed:(UIButton *)sender {
-    NSLog(@"An item was pressed!");
-    self.selectedItem = (Item *)sender;
-    if(![self verifySelectionView:self.selectedItem]){
-        self.subjectSelectionView.hidden = NO;
-        self.subjectSelectionView.layer.zPosition = 1;
-        //self.dismissView.hidden = NO;
-        [self updateSubjectSelectionView];
-    }
+
+
+- (IBAction)itemPressed:(UIButton *)sender{
     
-    //self.selectedItem = NULL;
-}
-
--(void)clearSubjectViewContent{
-    self.selectedItemName.text = nil;
-    self.selectedItemDescription.text = nil;
-    //self.selectedItemSubject1.titleLabel.hidden = YES;
-    self.selectedItemSubject2.titleLabel.text = @"";
-    self.selectedItemSubject3.titleLabel.text = nil;
-    self.selectedItemSubject4.titleLabel.text = nil;
-    self.selectedItemSubject5.titleLabel.text = nil;
-    self.selectedItemSubject6.titleLabel.text = nil;
-}
-
-- (BOOL)verifySelectionView:(Item *)item{
-    if(self.selectedItemName.text == item.itemName){
-        return YES;
+    if(self.selectedItem != (Item *)sender){
+        self.selectedItem = (Item *)sender;
     }
-    return NO;
+    [self showSubjectView];
+    
+
+}
+    
+- (void)showSubjectView{
+    [self updateSubjectView];
+    self.subjectView.layer.zPosition = 1;
+    self.subjectView.hidden = NO;
 }
 
--(void)updateSubjectSelectionView {
+- (void)updateSubjectView{
+    
     self.selectedItemName.text = self.selectedItem.itemName;
+    self.selectedItemDescription.text = nil;
+    [self setButtonName];
     
-    // We need to add a description for every item before uncommenting the line below.
-    //self.selectedItemDescription.text = self.selectedItem.itemDescription;
-    
-    switch ([self.selectedItem.itemSubjects count]) {
-        case 1:
-            self.selectedItemSubject1.titleLabel.text = [self.selectedItem.itemSubjects objectAtIndex:0].subjectName;
-            break;
-        case 2:
+}
+
+- (void)setButtonName{
+    NSUInteger index;
+    for(UIButton *current in self.buttonArray){
+        if([self.buttonArray indexOfObject:current] < [self.selectedItem.itemSubjects count]){
+            index = [self.buttonArray indexOfObject:current];
+            [current setEnabled:YES];
+            [current setTitle:[[self.selectedItem.itemSubjects objectAtIndex:index] subjectName] forState:UIControlStateNormal];
+            [current.titleLabel sizeToFit];
+        }else{
+            [current setEnabled:NO];
+            [current setTitle:@"" forState:UIControlStateDisabled];
             
-            
-            self.selectedItemSubject1.titleLabel.text = [self.selectedItem.itemSubjects objectAtIndex:0].subjectName;
-            self.selectedItemSubject2.titleLabel.text = [self.selectedItem.itemSubjects objectAtIndex:1].subjectName;
-            self.selectedItemSubject3.titleLabel.hidden = YES;
-            self.selectedItemSubject4.titleLabel.hidden = YES;
-            //self.selectedItemSubject1.titleLabel.text =
-            //self.selectedItemSubject2.titleLabel.text =
-            break;
-        case 3:
-            self.selectedItemSubject1.titleLabel.text = [self.selectedItem.itemSubjects objectAtIndex:0].subjectName;
-            self.selectedItemSubject2.titleLabel.text = [self.selectedItem.itemSubjects objectAtIndex:1].subjectName;
-            self.selectedItemSubject3.titleLabel.text = [self.selectedItem.itemSubjects objectAtIndex:2].subjectName;
-            break;
-        case 4:
-            self.selectedItemSubject1.titleLabel.text = [self.selectedItem.itemSubjects objectAtIndex:0].subjectName;
-            self.selectedItemSubject2.titleLabel.text = [self.selectedItem.itemSubjects objectAtIndex:1].subjectName;
-            self.selectedItemSubject3.titleLabel.text = [self.selectedItem.itemSubjects objectAtIndex:2].subjectName;
-            self.selectedItemSubject4.titleLabel.text = [self.selectedItem.itemSubjects objectAtIndex:3].subjectName;
-            break;
-        case 5:
-            self.selectedItemSubject1.titleLabel.text = [self.selectedItem.itemSubjects objectAtIndex:0].subjectName;
-            self.selectedItemSubject2.titleLabel.text = [self.selectedItem.itemSubjects objectAtIndex:1].subjectName;
-            self.selectedItemSubject3.titleLabel.text = [self.selectedItem.itemSubjects objectAtIndex:2].subjectName;
-            self.selectedItemSubject4.titleLabel.text = [self.selectedItem.itemSubjects objectAtIndex:3].subjectName;
-            self.selectedItemSubject5.titleLabel.text = [self.selectedItem.itemSubjects objectAtIndex:4].subjectName;
-            break;
-        case 6:
-            self.selectedItemSubject1.titleLabel.text = [self.selectedItem.itemSubjects objectAtIndex:0].subjectName;
-            self.selectedItemSubject2.titleLabel.text = [self.selectedItem.itemSubjects objectAtIndex:1].subjectName;
-            self.selectedItemSubject3.titleLabel.text = [self.selectedItem.itemSubjects objectAtIndex:2].subjectName;
-            self.selectedItemSubject4.titleLabel.text = [self.selectedItem.itemSubjects objectAtIndex:3].subjectName;
-            self.selectedItemSubject5.titleLabel.text = [self.selectedItem.itemSubjects objectAtIndex:4].subjectName;
-            self.selectedItemSubject6.titleLabel.text = [self.selectedItem.itemSubjects objectAtIndex:5].subjectName;
-            break;
+        }
     }
 }
 
-/*
-#pragma mark - Navigation
+
+
+
+
+//- (IBAction)itemPressed:(UIButton *)sender {
+//    NSLog(@"An item was pressed!");
+//    self.selectedItem = (Item *)sender;
+//    if(![self verifySelectionView:self.selectedItem]){
+//        self.subjectSelectionView.hidden = NO;
+//        self.subjectSelectionView.layer.zPosition = 1;
+//        //self.dismissView.hidden = NO;
+//        [self updateSubjectSelectionView];
+//    }
+//    
+//    //self.selectedItem = NULL;
+//}
+//
+//-(void)clearSubjectViewContent{
+//    self.selectedItemName.text = nil;
+//    self.selectedItemDescription.text = nil;
+//    //self.selectedItemSubject1.titleLabel.hidden = YES;
+//    self.selectedItemSubject2.titleLabel.text = @"";
+//    self.selectedItemSubject3.titleLabel.text = nil;
+//    self.selectedItemSubject4.titleLabel.text = nil;
+//    self.selectedItemSubject5.titleLabel.text = nil;
+//    self.selectedItemSubject6.titleLabel.text = nil;
+//}
+//
+//- (BOOL)verifySelectionView:(Item *)item{
+//    if(self.selectedItemName.text == item.itemName){
+//        return YES;
+//    }
+//    return NO;
+//}
+//
+//-(void)updateSubjectSelectionView {
+//    self.selectedItemName.text = self.selectedItem.itemName;
+//    
+//    // We need to add a description for every item before uncommenting the line below.
+//    //self.selectedItemDescription.text = self.selectedItem.itemDescription;
+//    
+//    switch ([self.selectedItem.itemSubjects count]) {
+//        case 1:
+//            self.selectedItemSubject1.titleLabel.text = [self.selectedItem.itemSubjects objectAtIndex:0].subjectName;
+//            break;
+//        case 2:
+//            
+//            
+//            self.selectedItemSubject1.titleLabel.text = [self.selectedItem.itemSubjects objectAtIndex:0].subjectName;
+//            self.selectedItemSubject2.titleLabel.text = [self.selectedItem.itemSubjects objectAtIndex:1].subjectName;
+//            self.selectedItemSubject3.titleLabel.hidden = YES;
+//            self.selectedItemSubject4.titleLabel.hidden = YES;
+//            //self.selectedItemSubject1.titleLabel.text =
+//            //self.selectedItemSubject2.titleLabel.text =
+//            break;
+//        case 3:
+//            self.selectedItemSubject1.titleLabel.text = [self.selectedItem.itemSubjects objectAtIndex:0].subjectName;
+//            self.selectedItemSubject2.titleLabel.text = [self.selectedItem.itemSubjects objectAtIndex:1].subjectName;
+//            self.selectedItemSubject3.titleLabel.text = [self.selectedItem.itemSubjects objectAtIndex:2].subjectName;
+//            break;
+//        case 4:
+//            self.selectedItemSubject1.titleLabel.text = [self.selectedItem.itemSubjects objectAtIndex:0].subjectName;
+//            self.selectedItemSubject2.titleLabel.text = [self.selectedItem.itemSubjects objectAtIndex:1].subjectName;
+//            self.selectedItemSubject3.titleLabel.text = [self.selectedItem.itemSubjects objectAtIndex:2].subjectName;
+//            self.selectedItemSubject4.titleLabel.text = [self.selectedItem.itemSubjects objectAtIndex:3].subjectName;
+//            break;
+//        case 5:
+//            self.selectedItemSubject1.titleLabel.text = [self.selectedItem.itemSubjects objectAtIndex:0].subjectName;
+//            self.selectedItemSubject2.titleLabel.text = [self.selectedItem.itemSubjects objectAtIndex:1].subjectName;
+//            self.selectedItemSubject3.titleLabel.text = [self.selectedItem.itemSubjects objectAtIndex:2].subjectName;
+//            self.selectedItemSubject4.titleLabel.text = [self.selectedItem.itemSubjects objectAtIndex:3].subjectName;
+//            self.selectedItemSubject5.titleLabel.text = [self.selectedItem.itemSubjects objectAtIndex:4].subjectName;
+//            break;
+//        case 6:
+//            self.selectedItemSubject1.titleLabel.text = [self.selectedItem.itemSubjects objectAtIndex:0].subjectName;
+//            self.selectedItemSubject2.titleLabel.text = [self.selectedItem.itemSubjects objectAtIndex:1].subjectName;
+//            self.selectedItemSubject3.titleLabel.text = [self.selectedItem.itemSubjects objectAtIndex:2].subjectName;
+//            self.selectedItemSubject4.titleLabel.text = [self.selectedItem.itemSubjects objectAtIndex:3].subjectName;
+//            self.selectedItemSubject5.titleLabel.text = [self.selectedItem.itemSubjects objectAtIndex:4].subjectName;
+//            self.selectedItemSubject6.titleLabel.text = [self.selectedItem.itemSubjects objectAtIndex:5].subjectName;
+//            break;
+//    }
+//}
+//
+///*
+//#pragma mark - Navigation
 
 // In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
+//- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+//    // Get the new view controller using [segue destinationViewController].
+//    // Pass the selected object to the new view controller.
+//}
+//*/
 
 @end
