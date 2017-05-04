9a4b46bc401902d32beb29aff72b2b882d45d93b
gmhertz <gmhertz@Gunters-MacBook-Pro.local>
2017-05-04 14:28:05 -0300

    plist bugs

--- miniChallenge/ViewController.m
+++ miniChallenge/ViewController.m
@@ -30,8 +30,6 @@
     userAux = [[currentUser sharedManager]user];
     //[[[currentUser sharedManager]user]changeMusicStatus];
     [[currentUser sharedManager]saveConfiguration];
-
-
     if([userAux isMusicOn]){
         [[AudioManager sharedManager]toggle];
     }
